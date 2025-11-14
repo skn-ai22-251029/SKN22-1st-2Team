14ea3b520814ebeb32812c3e2c16f5bd81dc92de
999ym <yeonmi47@gmail.com>
2025-11-14 15:14:04 +0900

    Feat: diff_charge.py 초기 정보 가져오는 버튼 삽입

--- pages/2_diff_charge.py
+++ pages/2_diff_charge.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import layout
+from services.get_charge_price import scrapping_charge_price
 
 layout.base_layout()
 
 st.write('요금 비교 페이지')
+if st.button('정보 가져오기!'):
+    scrapping_charge_price()
+    print('요금정보 등록 완료')
